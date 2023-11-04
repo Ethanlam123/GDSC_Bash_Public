aeedc9a5af10d11eddabbcc151cb208b18bbeb1d
Ethanlam <lamyusingethan@gmail.com>
2023-11-04 18:06:13 +0800

    add example

---
+++ example/for_loop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+NAMES="a b c d"
+for NAME in $NAMES
+do
+    echo "Hello $NAME"
+done
---
+++ example/function.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Define the function
+greet () {
+  echo "Hello, $1!"
+}
+# Call the function
+greet "Alice"
+greet "Bob"
